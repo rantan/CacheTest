a4a6edc5e3aa712ac9572c9315e042cd627a140d
k_taniguchi <k_taniguchi@haw.co.jp>
2014-10-17 01:36:09 +0900

    Initial Commit

---
+++ CacheTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CacheTest
+//
+//  Created by rantan on 2014/10/17.
+//  Copyright (c) 2014年 jp.co.haw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
